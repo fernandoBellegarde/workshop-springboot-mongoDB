022754906aa644e70dbf0305bc4d12431c25ec82
fernandoBellegarde <bellegardefernando@gmail.com>
2025-06-06 17:15:52 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/fernandobell/workshopmongo/config/Instantiation.java
+++ src/main/java/com/fernandobell/workshopmongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.fernandobell.workshopmongo.config;
 
 import com.fernandobell.workshopmongo.UserDTO.AuthorDTO;
+import com.fernandobell.workshopmongo.UserDTO.CommentDTO;
 import com.fernandobell.workshopmongo.domain.Post;
 import com.fernandobell.workshopmongo.domain.User;
 import com.fernandobell.workshopmongo.repository.PostRepository;
@@ -10,6 +11,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -36,8 +40,15 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018").toInstant(), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-        Post post2 = new Post(null, sdf.parse("23/03/2018").toInstant(), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/fernandobell/workshopmongo/domain/Post.java
+++ src/main/java/com/fernandobell/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.fernandobell.workshopmongo.domain;
 
 import com.fernandobell.workshopmongo.UserDTO.AuthorDTO;
+import com.fernandobell.workshopmongo.UserDTO.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -16,20 +19,23 @@ public class Post implements Serializable {
 
     @Id
     private String id;
-    private Instant date;
+    private Date date;
     private String title;
     private String body;
     private AuthorDTO author;
 
+    private List<CommentDTO> comments = new ArrayList<>();
+
     public Post() {}
 
-    public Post(String id, Instant date, String title, String body, AuthorDTO author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
         this.body = body;
         this.author = author;
     }
+
 
     public String getId() {
         return id;
@@ -40,11 +46,11 @@ public class Post implements Serializable {
         return this;
     }
 
-    public Instant getDate() {
+    public Date getDate() {
         return date;
     }
 
-    public Post setDate(Instant date) {
+    public Post setDate(Date date) {
         this.date = date;
         return this;
     }
@@ -75,6 +81,14 @@ public class Post implements Serializable {
         return this;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -86,5 +100,4 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
-
 }
